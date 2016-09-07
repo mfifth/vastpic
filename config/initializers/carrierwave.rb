8448c038342b259fa29062e40b4827df5ed1b321
mfifth <mfifth@gmail.com>
2016-09-06 18:28:43 -0700

    meh

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
-	CarrierWave.configure do |config|
-	  config.fog_credentials = {
-	    :provider               => 'AWS',                        # required
-	    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
-	    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                     # required
-	    :region                 => ENV['AWS_REGION']                  # optional, defaults to 'us-east-1'
-	  }
-	  config.fog_directory  = ENV.fetch('S3_BUCKET_NAME')                             # required
-	  config.s3_access_policy = :public_read
-	end
+CarrierWave.configure do |config|
+	 config.fog_credentials = {
+	  :provider               => 'AWS',                        # required
+	  :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
+	  :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                     # required
+	  :region                 => ENV['AWS_REGION']                  # optional, defaults to 'us-east-1'
+	}
+	 config.fog_directory  = ENV['S3_BUCKET_NAME']                             # required
+	 config.s3_access_policy = :public_read
+end
